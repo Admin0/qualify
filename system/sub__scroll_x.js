9cc3ad6d9514f3220fe7ad96232654321f88e2e0
JinH <jinh719@gmail.com>
2018-10-05 00:03:19 +0900

    update design

--- system/sub__scroll_x.js
+++ system/sub__scroll_x.js
@@ -1,17 +1,27 @@
-jQuery(function ($) {
-    $.fn.hScroll = function (amount) {
-        amount = amount || 120;
-        $(this).bind("DOMMouseScroll mousewheel", function (event) {
-            var oEvent = event.originalEvent,
-                direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta,
-                position = $(this).scrollLeft();
-            position += direction > 0 ? -amount : amount;
-            $(this).scrollLeft(position);
-            event.preventDefault();
-        })
-    };
+jQuery(function($) {
+  $.fn.hScroll = function(amount) {
+    amount = amount || 120;
+    $(this).bind("DOMMouseScroll mousewheel", function(event) {
+      var oEvent = event.originalEvent,
+        direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta,
+        position = $(this).scrollLeft();
+      // console.log(position);
+      position += direction > 0 ? -amount : amount;
+      if (position > 0) {
+        $('nav').css({
+          'box-shadow': '2px 0 6px 0 rgba(60, 64, 67, .15)'
+        });
+      } else {
+        $('nav').css({
+          'box-shadow': 'none'
+        });
+      }
+      $(this).scrollLeft(position);
+      event.preventDefault();
+    })
+  };
 });
 
 $(document).ready(function() {
-    $('.contents').hScroll(300); // You can pass (optionally) scrolling amount
+  $('.contents').hScroll(300); // You can pass (optionally) scrolling amount
 });
