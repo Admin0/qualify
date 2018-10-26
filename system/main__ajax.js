2a8c158da5e1d18e943dbd1a901308bd711dff48
JinH <jinh719@gmail.com>
2018-10-26 18:14:24 +0900

    1.03

--- system/main__ajax.js
+++ system/main__ajax.js
@@ -1,10 +1,10 @@
 const ad =
   '<section class="ad">' +
-  '<!-- 반응형 -->' +
+  '<!-- qualify 반응형 -->' +
   '<ins class="adsbygoogle"' +
   '     style="display:block"' +
   '     data-ad-client="ca-pub-8175591114279139"' +
-  '     data-ad-slot="6064236981"' +
+  '     data-ad-slot="5452657644"' +
   '     data-ad-format="auto"' +
   '     data-full-width-responsive="true"></ins>' +
   '<script>' +
