855a490e240b4ce36454d99ef68a0e80c5be1e56
JinH <jinh719@gmail.com>
2018-11-02 00:40:37 +0900

    1.04

--- system/main__card.js
+++ system/main__card.js
@@ -1,15 +1,15 @@
 const card_ad =
-  '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' +
-  '<!-- qualify 반응형 -->' +
   '<ins class="adsbygoogle"' +
   '     style="display:block"' +
   '     data-ad-client="ca-pub-8175591114279139"' +
   '     data-ad-slot="5452657644"' +
   '     data-ad-format="auto"' +
-  '     data-full-width-responsive="true"></ins>' +
-  '<script>' +
-  '(adsbygoogle = window.adsbygoogle || []).push({});' +
-  '</script>' ;
+  '     data-full-width-responsive="true"></ins>';
+
+var card_ad_1 = document.createElement('script');
+card_ad_1.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+card_ad_1.async = true;
+document.head.appendChild(card_ad_1);
 
 const is_card = document.querySelector("#main_item") == null;
 
@@ -22,5 +22,7 @@ if (is_card) {
   const card_title_html = '<h1 id="card_title">' + card_title + '</h1>'
   const card_footer_html = '<div id="card_footer">https://' + window.location.host + '/qualify/</div>'
   document.querySelector('.contents').insertAdjacentHTML('beforebegin', card_title_html + card_ad);
+  (adsbygoogle = window.adsbygoogle || []).push({});
   document.querySelector('.contents').insertAdjacentHTML('afterend', card_ad + card_footer_html);
+  (adsbygoogle = window.adsbygoogle || []).push({});
 }
