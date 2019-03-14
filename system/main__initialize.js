3046d7c2af79ab21c4d065d43855174e9ccf3585
JinH <jinh719@gmail.com>
2019-03-14 17:54:53 +0900

    1.10

--- system/main__initialize.js
+++ system/main__initialize.js
@@ -15,7 +15,7 @@ function load_navigat_items(category, item) {
     "name": category,
     "round": item,
     html: "<a href=#" + category + "-" + item + " onclick=" + onclick + "><i class='material-icons'>book</i><span>" + item + "<span></a>"
-  }).appendTo("#nav_item_list").append("<a href='" + category + "/" + item + ".html' target='_blank'><i class='material-icons'>open_in_new</i></a>");
+  }).appendTo("#nav_item_list");
 }
 
 function load_content_title(key, val) {
@@ -34,7 +34,7 @@ function load_content_items(category, item) {
     "name": category,
     "round": item,
     html: "<h3 onclick=" + onclick + "><a href=#" + category + "-" + item + " onclick=" + onclick + "><i class='material-icons'>book</i><span>" + item + "<span></a></h3>"
-  }).appendTo("#item_list").append("<a href='" + category + "/" + item + ".html' target='_blank'><i class='material-icons'>open_in_new</i></a>");
+  }).appendTo("#item_list");
 }
 
 function initialize() {
