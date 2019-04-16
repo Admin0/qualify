f83a3e022c168d5339e8a0e8187c5ad24d3988d2
JinH <jinh719@gmail.com>
2019-04-17 05:17:46 +0900

    1.21.09 분석기사 작업형 개선.

--- system/main__initialize_locked.js
+++ system/main__initialize_locked.js
@@ -1,15 +1,15 @@
 // console.log("loaded main__initialize_locked.js");
 
 $("body").keyup(function(event) {
-  if ($("[locked]").length != 0 && $("#setting").hasClass("on")) {
+  if ($("[locked=true]").length != 0 && $("#setting").hasClass("on")) {
     if (event.which == 81) { // q
       // console.log(event);
-      $("[locked]").each(function() {
+      $("[locked=true]").each(function() {
         $(this).attr("onclick", "load('" + $(this).attr("name") + "','" + $(this).attr("round") + "')");
         // console.log(this);
       });
-      $("[locked] i").text("lock_open");
-      $("[locked]").removeAttr("locked");
+      $("[locked=true] i").text("lock_open");
+      $("[locked=true]").removeAttr("locked");
 
       toast("관리자 모드가 활성화되었습니다.");
     }
