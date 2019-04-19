c0b84dfeb1e4226dc18c111bf41360c9e6208a4a
JinH <jinh719@gmail.com>
2019-04-19 22:34:34 +0900

    1.22.02 화공기사 문제 추가. 콘솔 시스템 개선.

--- system/main__initialize_console.js
+++ system/main__initialize_console.js
@@ -15,8 +15,7 @@ $("body").keyup(function(event) {
           });
           $("[locked=true] i").text("lock_open");
           $("[locked=true]").removeAttr("locked");
-
-          toast("잠긴 문서가 해제되었습니다.");
+          toast("잠긴 문서가 해제되었습니다.", get_icon("lock_open"));
           break;
         }
       case 65: // a
@@ -31,40 +30,19 @@ $("body").keyup(function(event) {
   } else {
     switch (event.which) {
       case 83: // s
-        $('#setting, #setting_bg').toggleClass('on');
-        $('#setting').css({
-          "top": $("#setting_bt").offset().top - pageYOffset,
-          "left": $("#nav").width() - 16
-        });
-        check_setting();
-        $('#context_menu').removeClass('on');
+        console_event("s");
         break;
       case 82: // r
-        location.reload();
+        console_event("r");
         break;
       case 79: // o
-        var t = $("section.targeted");
-        var url = "/qualify/" + t.attr("name") + "/" + t.attr("round") + ".html"
-        console.log(t.attr("name") + "-" + t.attr("round"));
-        window.open(url, "_blank");
+        console_event("o");
         break;
       case 81: // q
-        $('#main_item .contents.on').remove();
-        $('#title').text('#qualify');
-        $('nav, #nav_bg').removeClass('on');
+        console_event("q");
         break;
       case 70: // f
-        if (!$("#item_list h2").hasClass("slided")) {
-          $("#item_list h2").each(function() {
-            slide($(this).attr("id"), true);
-            toast("목록을 모두 접었습니다.");
-          });
-        } else {
-          $("#item_list h2").each(function() {
-            slide($(this).attr("id"), false);
-            toast("목록을 모두 펼쳤습니다.");
-          });
-        }
+        console_event("f");
         break;
       default:
 
@@ -72,4 +50,46 @@ $("body").keyup(function(event) {
   }
 });
 
+function console_event(code) {
+  switch (code) {
+    case "s":
+      $('#setting, #setting_bg').toggleClass('on');
+      $('#setting').css({
+        "top": $("#setting_bt").offset().top - pageYOffset,
+        "left": $("#nav").width() - 16
+      });
+      check_setting();
+      $('#context_menu').removeClass('on');
+      break;
+    case "q":
+      $('#main_item .contents.on').remove();
+      $('#title').text('#qualify');
+      $('nav, #nav_bg').removeClass('on');
+      break;
+    case "f":
+      if (!$("#item_list h2").hasClass("slided")) {
+        $("#item_list h2").each(function() {
+          slide($(this).attr("id"), true);
+          toast("목록을 모두 접었습니다.", "unfold_less");
+        });
+      } else {
+        $("#item_list h2").each(function() {
+          slide($(this).attr("id"), false);
+          toast("목록을 모두 펼쳤습니다.", "unfold_more");
+        });
+      }
+      break;
+    case "o":
+      var t = $("section.targeted");
+      var url = "/qualify/" + t.attr("name") + "/" + t.attr("round") + ".html"
+      console.log(t.attr("name") + "-" + t.attr("round"));
+      window.open(url, "_blank");
+      break;
+    case "r":
+      location.reload(true);
+      break;
+    default:
+
+  }
+}
 // console.log("loaded main__initialize_console.js");
